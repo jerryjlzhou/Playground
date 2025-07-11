bcca221c55d04b1bca99b8c6259b78364ed1999b
Jerry Zhou <jerry.zhou25@gmail.com>
2025-07-11 20:55:58 +1000

    Added height percentage and width percentage helpers

---
+++ constants/theme.js
@@ -0,0 +1,35 @@
+export const theme = {
+    colors: {
+        red: '#FF1744',
+        redDark: '#D50000',
+        gray: '#e3e3e3',
+        darkGray: '#757575',
+
+        emerald: '#10B981',
+        darkEmerald: '#047857',
+        
+        text: '#000000',
+        textLight: '#7C7C7C',
+        textDark: '#1D1D1D',
+
+        rose: '#ef4444',
+        roseLight: '#f87171'
+
+    },
+    fonts: {
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+    },
+
+    radius: {
+        xs: 10,
+        sm: 12,
+        md: 14,
+        lg: 16,
+        xl: 18,
+        xxl: 22,
+        circle: 999
+    }
+}
---
+++ helpers/common.js
@@ -0,0 +1,17 @@
+import { Dimensions } from "react-native";
+
+const {width: deviceWidth, height: deviceHeight} = Dimensions.get('window');
+
+// height perccentage
+export const hp = percentage => {
+    return (percentage * deviceHeight) / 100;
+}
+
+// width percentage
+ export const wp = percentage => {
+    return (percentage * deviceWidth) / 100;
+}
+
+export const stripHtmlTags = (html) => {
+    return html.replace(/<[^>]*>?/gm, '');
+}
